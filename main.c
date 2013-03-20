b1db26d4ab7002a09e17e73390004d2d97265538
Per Karlstedt <per.karlstedt@gmail.com>
2013-03-20 16:58:11 +0100

    Added IPC over SPI

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "m128_hal.h"
-#define LED_PIN 1
+
 #define CS_MSK 0x04
 #define CS_MSK 0x04
 #define CLK_MSK 0x20
@@ -14,56 +14,63 @@
 #define GLED_PIN PD3
 #define GLED_DDR DDRD
 #define GLED_PORT PORTD
+
 #define GLED_INIT() (GLED_DDR|=(1<<GLED_PIN))
 #define GLED_SET() (GLED_PORT|=(1<<GLED_PIN))
+#define GLED_TOGGLE() (GLED_PORT^=(1<<GLED_PIN))
 #define GLED_CLR() (GLED_PORT&=~(1<<GLED_PIN))
+
+#define BLED_PIN 1
+#define BLED_DDR DDRB
+#define BLED_PORT PORTB
+
+#define BLED_INIT() (BLED_DDR|=(1<<BLED_PIN))
+#define BLED_SET() (BLED_PORT|=(1<<BLED_PIN))
+#define BLED_TOGGLE() (BLED_PORT^=(1<<BLED_PIN))
+#define BLED_CLR() (BLED_PORT&=~(1<<BLED_PIN))
+
 
 #define IRQ_PIN PD0
 #define SPI_CLK PB5
+
+volatile uint8_t ipc_rcv_buf = 0;
+
 ISR(SPI_STC_vect)
 {
-    volatile char val = SPSR;
-    volatile char vali = SPDR;
-    SPCR = (1<<SPIE) | (1<<SPE) | (1<<MSTR);
-    val++;
-    vali++;
-    PORTB |= (1<<LED_PIN);
-    //PORTD |= (1<<IRQ_PIN);
-    _delay_ms(5);
-    PORTB &= ~(1<<LED_PIN);
-    //PORTD &= ~(1<<IRQ_PIN);
+    ipc_rcv_buf = SPDR;
+    BLED_SET();
+    _delay_ms(1);
+    BLED_CLR();
 }
 
 int main(void)
 {
+    SPCR = (1<<SPIE) | (1<<SPE);
+    /* Init LED pins */
+    BLED_INIT();
     GLED_INIT();
+
+    /* Blink green led as boot indication */
     GLED_SET();
-    _delay_ms(5000);
+    _delay_ms(100);
     GLED_CLR();
     
+    /* Enable global interrupts */    
     STATUS_REGISTER |= (1<<STATUS_REGISTER_IT);
     
-    /* Set LED as output */
-    DDRB |= (1<<LED_PIN);
-    
-    /* Set CS and CLK as input */
-    //DDRB &= ~(CS_MSK);
-    //DDRB &= ~(1<<SPI_CLK);
-    //PORTB &= ~(CS_MSK);
-    //PORTB |= (1<<SPI_CLK);
+    /* Do we need to set MISO as output? */
+    DDRB |= (1<<PB4);
 
     /* Set IRQ as output */
-    DDRD |= (1<<IRQ_PIN);
+    //DDRD |= (1<<IRQ_PIN);
 
     /* Init SPI Slave */
-    SPCR = (1<<SPIE) | (1<<SPE);// | (1<<MSTR);
-    //SPDR = 0xCC;
     while(1)
     {
-        GLED_SET();
-        _delay_ms(100);
-        GLED_CLR();
-        _delay_ms(5000);
+        if (ipc_rcv_buf > 0x01 && ipc_rcv_buf < 20)
+            GLED_SET();
+        else
+            GLED_CLR();
     }
 
     return 0;
