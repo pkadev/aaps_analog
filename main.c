b8e723f59c1420bb1ed952cc0bf06caa6b069359
Per Karlstedt <per.karlstedt@gmail.com>
2013-03-26 20:04:56 +0100

    Temporary commit for branching

--- main.c
+++ main.c
@@ -13,13 +13,23 @@
 #define CLK_MSK 0x20 //What the hell are you?
 
 //REMOVE
-#define RLED_PIN 1
+#define RLED_PIN 6
 #define RLED_DDR DDRD
 #define RLED_PORT PORTD
 #define RLED_INIT() (RLED_DDR|=(1<<RLED_PIN))
 #define RLED_SET() (RLED_PORT|=(1<<RLED_PIN))
 #define RLED_CLR() (RLED_PORT&=~(1<<RLED_PIN))
 // END REMOVE
+
+/* DAC Current defines */
+#define CS_DAC_STR_PIN 1
+#define CS_DAC_STR_DDR DDRC
+#define CS_DAC_STR_PORT PORTC
+#define CS_DAC_STR_INIT() (CS_DAC_STR_DDR|=(1<<CS_DAC_STR_PIN))
+#define CS_DAC_STR_SET() (CS_DAC_STR_PORT|=(1<<CS_DAC_STR_PIN))
+#define CS_DAC_STR_CLR() (CS_DAC_STR_PORT&=~(1<<CS_DAC_STR_PIN))
+
+/* End DAC current defines */
 
 #define RELAY_D_PIN 7
 #define RELAY_D_DDR DDRD
@@ -162,26 +172,103 @@ enum ipc_command_t
     IPC_CMD_GET_VOLTAGE,
     IPC_CMD_GET_CURRENT
 };
+#define SPI_WAIT() while((SPSR & (1<<SPIF)) != (1<<SPIF))
+    #define IPC_CMD_DATA_AVAILABLE 0x10
+static void print_ipc(const char *str)
+{
+    uint8_t len = strlen(str);
+    SPCR &= ~(1<<SPIE);
+    
+    /* Put CMD in SPI data buffer */
+    SPDR = IPC_CMD_DATA_AVAILABLE;
+
+    /* Signal to master that CMD is available */
+    IRQ_SET();
+    SPI_WAIT();
+    _delay_ms(10); //TODO: Used to debug synchronization. Remove!!
+    IRQ_CLR();
+
+    /* Tell master how many bytes to fetch */
+    SPDR = len+1;
+
+    _delay_ms(10); //TODO: Used to debug synchronization. Remove!!
+    IRQ_SET();
+    SPI_WAIT();
+    _delay_ms(10); //TODO: Used to debug synchronization. Remove!!
+    IRQ_CLR();
+
+    for(uint8_t i = 0; i <= len; i++) {
+        SPDR = str[i];
+        _delay_ms(10); //TODO: Used to debug synchronization. Remove!!
+        IRQ_SET();
+        
+        SPI_WAIT();
+        _delay_ms(10); //TODO: Used to debug synchronization. Remove!!
+        IRQ_CLR();
+        
+    }
+ 
+    SPCR |= (1<<SPIE);
+}
+static void init_spi(void)
+{
+    UBRR0 = 150; 
+    UCSR0B |= (1<<RXEN0) | (1<<TXEN0);
+    UCSR0C |= (1<<UMSEL01) | (1<<UMSEL00) | (1<<UCPOL0);
+    DDRD |= (1<<PD4);
+}
+
+static uint8_t spi_send(uint8_t xfer)
+{
+    CS_DAC_STR_CLR();
+    _delay_ms(50);
+    UDR0 = xfer;
+    _delay_ms(50);
+    CS_DAC_STR_SET();
+    if(UDR0 == 0)
+        return 'R';
+    return UDR0;
+}
+
 int main(void)
 {
+    _delay_ms(200);
 
     SPCR = (1<<SPIE) | (1<<SPE) | (1<<CPHA);
     enable_ext_irq0();
     enable_pcint18(); 
     enable_pcint0();
-
+    init_spi();
     /* Init LED pins */
     RELAY_D_INIT();
     LED_INIT();
     RLED_INIT();
     IRQ_INIT();
-
+    CS_DAC_STR_INIT();
+    char str[10] = {0};
     /* Enable global interrupts */    
     STATUS_REGISTER |= (1<<STATUS_REGISTER_IT);
-    
+    uint8_t data1, data2;    
     /* Do we need to set MISO as output? */
     DDRB |= (1<<PB4);
 
+    str[0] = 'C';
+    str[1] = 0;
+    while(1)
+    {
+        spi_send(0xCC);
+        spi_send(0x55);
+    }
+    str[3] = '\0';
+    if (data1 != 0)
+        str[1] = data1;
+    if (data1 != 0)
+        str[2] = data2;
+    print_ipc(str);
+    _delay_ms(10);
+_delay_ms(10);
+    //print_ipc(str);//    RLED_SET();
+ 
     while(1)
     {
         if (ipc_rcv_buf == IPC_CMD_PERIPH_DETECT) {
