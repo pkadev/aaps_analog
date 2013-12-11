c81da0ba2a028354891d70227d70ae852cf4b689
Per Karlstedt <per.karlstedt@gmail.com>
2013-12-11 17:13:56 +0100

    Refactoring

--- cmd_exec.c
+++ cmd_exec.c
@@ -78,3 +78,37 @@ aaps_result_t cmd_exec_ctrl_relay(struct ipc_packet_t *packet,
 
     return AAPS_RET_OK;
 }
+aaps_result_t core_handle_ipc_pkt(struct ipc_packet_t *pkt)
+{
+    aaps_result_t res = AAPS_RET_OK;
+    switch(pkt->cmd)
+    {
+        case IPC_CMD_PERIPH_DETECT:
+            //print_ipc("[A] P detect\n");
+            break;
+        case IPC_CMD_SET_VOLTAGE:
+            write_voltage(pkt->data[1], pkt->data[0]);
+            break;
+        case IPC_CMD_SET_CURRENT_LIMIT:
+            write_current_limit(pkt->data[1], pkt->data[0]);
+            break;
+        case IPC_CMD_GET_ADC:
+            cmd_exec_get_adc(pkt);
+            break;
+        case IPC_CMD_GET_TEMP:
+            cmd_exec_get_temp(pkt);
+            break;
+        case IPC_CMD_SET_RELAY_D:
+            cmd_exec_ctrl_relay(pkt, RELAY_D_ID);
+            break;
+        case IPC_CMD_SET_RELAY:
+            cmd_exec_ctrl_relay(pkt, RELAY_ID);
+            break;
+        default:
+            //print_ipc_int("[A] Unknown ipc command 0x", ipc_packet->cmd);
+            return AAPS_RET_ERROR_GENERAL;
+        break;
+    }
+
+   return res;
+}
--- core.c
+++ core.c
@@ -5,40 +5,6 @@
 #include "cmd_exec.h"
 #include "mspim.h"
 
-aaps_result_t core_handle_ipc_pkt(struct ipc_packet_t *pkt)
-{
-    aaps_result_t res = AAPS_RET_OK;
-    switch(pkt->cmd)
-    {
-        case IPC_CMD_PERIPH_DETECT:
-            //print_ipc("[A] P detect\n");
-            break;
-        case IPC_CMD_SET_VOLTAGE:
-            write_voltage(pkt->data[1], pkt->data[0]);
-            break;
-        case IPC_CMD_SET_CURRENT_LIMIT:
-            write_current_limit(pkt->data[1], pkt->data[0]);
-            break;
-        case IPC_CMD_GET_ADC:
-            cmd_exec_get_adc(pkt);
-            break;
-        case IPC_CMD_GET_TEMP:
-            cmd_exec_get_temp(pkt);
-            break;
-        case IPC_CMD_SET_RELAY_D:
-            cmd_exec_ctrl_relay(pkt, RELAY_D_ID);
-            break;
-        case IPC_CMD_SET_RELAY:
-            cmd_exec_ctrl_relay(pkt, RELAY_ID);
-            break;
-        default:
-            //print_ipc_int("[A] Unknown ipc command 0x", ipc_packet->cmd);
-            return AAPS_RET_ERROR_GENERAL;
-        break;
-    }
-
-   return res;
-}
 
 aaps_result_t core_send_ipc_adc_value(uint16_t val, uint8_t type, int8_t ch)
 {
